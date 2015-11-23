8c73f30063e37b71e1b3d6f12e69b55cbb233d30
hafenr <robin.hafen@gmail.com>
2015-11-23 12:05:51 +0100

    Fix typo and remove unused code

--- R/slidingWindowCorrelation.R
+++ R/slidingWindowCorrelation.R
@@ -42,41 +42,7 @@ computeWindowedCorrelation <- function(trace.mat, start.window.idx, window.size)
 #' @export
 slidingWindowCorrelation <- function(trace.mat, window.size) {
     corr <- sapply(seq(1, ncol(trace.mat)), function(i) {
-        computeWindowedCorrelation(tace.mat, i, window.size)
+        computeWindowedCorrelation(trace.mat, i, window.size)
     })
     corr
 }
-
-# protein.traces.with.complex <- produceComplexAnnotatedProteinTraces(
-#     '~/Dev/MAScripts/data/corum_complex_protein_assoc.tsv',
-#     '~/Dev/MAScripts/data/4_Fixed_weights_wMS1/Subsetted_OSW_Output/Peptide_Matrices/Long_Lists/4_osw_output_mscore_lt_1percent_no_requant_no_decoy_FILTERED.tsv'
-# )
-
-# protein.traces.subs <- protein.traces.with.complex[complex_id == complex.id, ]
-# protein.traces.subs[, n_proteins_in_complete_complex := NULL]
-
-# protein.traces.subs.wide <-
-#     dcast(protein.traces.subs, complex_id + protein_id + complex_name ~ sec,
-#           value.var='intensity')
-# mat <- as.matrix(subset(protein.traces.subs.wide, select=-c(1, 2, 3)))
-
-# window.size <- 10
-
-
-# corr.long <- data.table(corr=corr, sec=seq(min(protein.traces.subs$sec),
-#                                            max(protein.traces.subs$sec)),
-#                         complex_id=complex.id)
-
-# max.intensity <- max(protein.traces.subs$intensity)
-# p <- ggplot(protein.traces.subs) +
-#             geom_point(aes(x=sec, y=intensity, color=protein_id)) +
-#             geom_line(aes(x=sec, y=intensity, color=protein_id)) +
-#             geom_line(aes(x=sec, y=corr * max.intensity/2), color='black',
-#                       data=corr.long) +
-#             geom_abline(intercept=max.intensity/2, linetype=2)
-
-# print(p)
-
-
-
-
