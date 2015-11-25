1bad7f326fb20a56d7337b26e480e4b099a04d02
hafenr <robin.hafen@gmail.com>
2015-11-25 11:32:42 +0100

    Add functions to convert from wide to long formats and vv

--- R/dataInput.R
+++ R/dataInput.R
@@ -6,7 +6,7 @@
 #'        This table must have the columns: 'complex_id', 'protein_id'.
 #' @return A data.table of protein intensity observations with. 
 #'         The DT will have the columns: 'protein_id', 'sec', 'intensity',
-#'         'complex_id', and 'complex_name'.
+#'         'complex_id', 'complex_name'.
 #' @examples
 #' produceComplexAnnotatedProteinTraces(e4.peptide.traces.wide.filtered,
 #'                                      'corum_complex_protein_assoc.tsv')
@@ -27,11 +27,11 @@ produceComplexAnnotatedProteinTraces <- function(peptide.traces,
 
     # Add a new column to the corum proteins that tells how many proteins
     # are theoretically in the corum complex.
-    corum.protein.assoc[
-        ,
-        n_proteins_in_complete_complex := length(unique(protein_id)),
-        by=complex_id
-    ]
+    # corum.protein.assoc[
+    #     ,
+    #     n_proteins_in_complete_complex := length(unique(protein_id)),
+    #     by=complex_id
+    # ]
 
     # Merge protein traces together with a list of corum complexes
     # so that each intensity observation is annotated with the complex id
@@ -54,4 +54,29 @@ produceComplexAnnotatedProteinTraces <- function(peptide.traces,
 widePepTracesToLong <- function(dt) {
     melt(dt, id.var=c('protein_id', 'peptide_id'),
          variable.name='sec', value.name='intensity')
+}
+
+#' Convert a long list peptide trace to a wide format.
+#' export
+longPepTracesToWide <- function(dt) {
+    dcast(dt,
+          protein_id + peptide_id ~ sec,
+          value.var='intensity')
+}
+
+#' Convert a long list protein trace to a wide format.
+#' export
+longProtTracesToWide <- function(dt) {
+    dcast(dt,
+          protein_id + complex_id + complex_name ~ sec,
+          value.var='intensity')
+}
+
+#' Convert a wide format protein trace data.table to long format.
+#' @export
+wideProtTracesToLong <- function(dt) {
+    melt(dt,
+         id.var=c('protein_id', 'complex_id', 'complex_name'),
+         variable.name='sec',
+         value.name='intensity')
 }
