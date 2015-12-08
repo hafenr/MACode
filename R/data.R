f13a05623ec216512e604a51115b6a58b65cc9df
hafenr <robin.hafen@gmail.com>
2015-12-08 16:01:50 +0100

    Add dataset and new docs

--- R/data.R
+++ R/data.R
@@ -28,6 +28,27 @@
 #'  \item no decoy values
 #' }
 #'
+#' @format A data.table with 83266 rows and the following columns:
+#' \itemize {
+#'  \item protein_id: the id of the protein to which this peptide might belong
+#'  \item peptide_id: the id of the peptide itself
+#'  \item 3: intensity measurements for sec fraction 3
+#'  \item 4: intensity measurements for sec fraction 4
+#'  \item ...: further measurements
+#'  \item 83: intensity measurements for sec fraction 84
+#' }
+'e4.peptide.traces.wide'
+
+
+#' A wide format data.table of peptide trace intensities.
+#'
+#' The settings applied to the raw data are:
+#' \itemize {
+#'  \item m_score < 0.01
+#'  \item no requant values
+#'  \item no decoy values
+#' }
+#'
 #' Further filters were applied:
 #' \itemize {
 #'  \item require_at_least_x_peptides(df, 2)
@@ -68,3 +89,31 @@
 #' did not take part in the peak group.
 'manual.annotations.partial'
 
+
+#' The raw manual complex annotations by rhafen.
+#' These annotations are based on the traces
+#' summed up protein traces from 
+#' e4.peptide.traces.wide.filtered.
+#' A already processed and merged  version exists in the data frame
+#' \itemize {
+#'  \item manual.annotations.partial
+#'  \item manual.annotations.full
+#'  \item manual.annotations.full.complete
+#' }
+'manual.annotations.raw.rhafen'
+
+#' The raw manual complex annotations by mheusel
+#' These annotations are based on the traces
+#' summed up protein traces from 
+#' e4.peptide.traces.wide.filtered.
+#' A already processed and merged  version exists in the data frame
+#' \itemize {
+#'  \item manual.annotations.partial
+#'  \item manual.annotations.full
+#'  \item manual.annotations.full.complete
+#' }
+'manual.annotations.raw.mheusel'
+
+#' Manual annotations of partial peak groups that whose completeness
+#' was estimated by hierchical clustering of the trace correlation profiles.  
+manual.annotations.partial.imputed.win14.corr0.7
